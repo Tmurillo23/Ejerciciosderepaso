ced42b02d04c128f330496ddcfd5b836e36839cf
Tmurillo <tatianamurillomosquera1@gmail.com>
2023-08-01 18:46:46 -0500

    ejercicio_5 updated

--- ejerciosderepaso/ejercicio_5.py
+++ ejerciosderepaso/ejercicio_5.py
@@ -1,6 +1,7 @@
 def fahrenheit_to_celsius(num_1):
-    return (num_1-32.5)/1.8
+    return (num_1 - 32.5) / 1.8
+
 
 temperatura = fahrenheit_to_celsius(int(input("Diga la temperatura en Fahrenheit: ")))
 
-print(temperatura,"grados Celsius")
+print(temperatura, "grados Celsius")
