49713423c72f63f96423919cfc3aab0c07bb1a57
Tmurillo <tatianamurillomosquera1@gmail.com>
2023-08-01 18:06:39 -0500

    ejercicio_1 updated

--- ejerciosderepaso/ejercicio_1.py
+++ ejerciosderepaso/ejercicio_1.py
@@ -1,4 +1,3 @@
-def nombre_edad():
-    nombre = input("Ingrese su nombre: ")
-    edad = int(input("Ingrese su edad: "))
-    print(f"Su nombre es: {nombre} y su edad es: {edad}")
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print(f"Su nombre es: {nombre} y su edad es: {edad} años")
