43b73b6a59c922aeca639bc156a10327aad39fe8
Tmurillo <tatianamurillomosquera1@gmail.com>
2023-08-01 20:40:12 -0500

    ejercicio_13 done

---
+++ ejerciosderepaso/ejercicio_13.py
@@ -0,0 +1,10 @@
+num_1 = int(input("Ingrese un numero: "))
+num_2 = int(input("Ingrese un numero: "))
+suma = num_1 + num_2
+multiplicacion = num_1 * num_2
+resta = num_1 - num_2
+division = num_1/num_2
+print(f"{num_1} + {num_2} = {suma}")
+print(f"{num_1} - {num_2} = {resta}")
+print(f"{num_1} x {num_2} = {multiplicacion}")
+print(f"{num_1} / {num_2} = {division}")
